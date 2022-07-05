01311066e0acd6bdc457fe1e8f6df695b6f4166f
ksmnsmarts <ksm@nsmarts.co.kr>
2022-07-05 11:20:48 +0900

    일단 학생리스트 페이지 완료...

--- src/app/pages/class-management/comclass-student/comclass-student.component.ts
+++ src/app/pages/class-management/comclass-student/comclass-student.component.ts
@@ -85,7 +85,6 @@ export class ComclassStudentComponent implements OnInit {
 
         this.studentInfoService.currentStudent.pipe(takeUntil(this.unsubscribe$)).subscribe(
             (res: any) => {
-                console.log(res)
                 this.studentCount = res;
             }
         );
@@ -108,39 +107,30 @@ export class ComclassStudentComponent implements OnInit {
 
 
         /************************************************************
-        * 학생들이 현재 바라보고 있는 문서 정보
+        * 학생 리스트에 들어왔을 때 학생들이 현재 바라보고 있는 문서 페이지 보여주기
         ************************************************************/
         this.socket.emit('studentList:docInfo');
 
         this.socket.on('studentList:sendDocInfo', async (data) => {
-            console.log('111111111111111111111111111111111')
-           
-            console.log(data)
-            await new Promise(res => setTimeout(res, 300));
-            this.studentDocInfo = data;
-        })
 
-
-
-
-        /************************************************************
-         * 모니터링
-         ************************************************************/
-        this.socket.emit('begin:monitoring', '');
-
-        this.socket.on('send:monitoringCanvas', async (data) => {
-
-            for (let i = 0; i < this.studentList.length; i++) {
-                if (this.studentList[i].studentName == data.studentName)
-                    this.studentList[i].pageInfo = data.pageInfo
+            await new Promise(res => setTimeout(res, 500));
+            for (let i = 0; i < this.thumbArray.length; i++) {
+                if (this.thumbArray[i].studentName == data.studentName) {
+                    this.thumbArray[i].currentDocId = data.currentDocId;
+                    this.thumbArray[i].currentDocNum = data.currentDocNum;
+                    this.thumbArray[i].currentPage = data.currentPage;
+                }
             }
 
             const canvas = (document.getElementById('student_monitoring' + data.studentName) as HTMLInputElement);
             const studentImgBg = (document.getElementById('studentBg' + data.studentName) as HTMLInputElement);
 
-            const viewport = this.pdfStorageService.getViewportSize(data.pageInfo.currentDocNum, data.pageInfo.currentPage);
+            const viewport = this.pdfStorageService.getViewportSize(data.currentDocNum, data.currentPage);
 
 
+            console.log(this.thumbArray)
+
+            await new Promise(res => setTimeout(res, 500));
             // landscape 문서 : 가로를 300px(studentListMaxSize)로 설정
             if (viewport.width > viewport.height) {
                 canvas.width = CANVAS_CONFIG.studentListMaxSize;
@@ -159,16 +149,54 @@ export class ComclassStudentComponent implements OnInit {
             }
 
 
+            await this.renderingService.renderThumbBackground(studentImgBg, data.currentDocNum, data.currentPage);
+            await this.renderingService.renderThumbBoard(canvas, data.currentDocNum, data.currentPage);
+        })
+
+
+
+
+        /************************************************************
+         * 학생이 보고 있는 문서 페이지가 업데이트 됐을 때 업데이트 된 페이지 보여주기
+         ************************************************************/
+        this.socket.on('send:monitoringCanvas', async (data) => {
+            for (let i = 0; i < this.studentList.length; i++) {
+                if (this.studentList[i].studentName == data.studentName)
+                    this.studentList[i].pageInfo = data.pageInfo
+            }
+
+            const canvas = (document.getElementById('student_monitoring' + data.studentName) as HTMLInputElement);
+            const studentImgBg = (document.getElementById('studentBg' + data.studentName) as HTMLInputElement);
+            const viewport = this.pdfStorageService.getViewportSize(data.pageInfo.currentDocNum, data.pageInfo.currentPage);
+
+            await new Promise(res => setTimeout(res, 0));
+            // landscape 문서 : 가로를 300px(studentListMaxSize)로 설정
+            if (viewport.width > viewport.height) {
+                canvas.width = CANVAS_CONFIG.studentListMaxSize;
+                canvas.height = canvas.width * viewport.height / viewport.width;
+
+                studentImgBg.width = CANVAS_CONFIG.studentListMaxSize;
+                studentImgBg.height = studentImgBg.width * viewport.height / viewport.width;
+            }
+            // portrait 문서 : 세로를 300px(studentListMaxSize)로 설정
+            else {
+                canvas.height = CANVAS_CONFIG.studentListMaxSize;
+                canvas.width = canvas.height * viewport.width / viewport.height;
+
+                studentImgBg.height = CANVAS_CONFIG.studentListMaxSize;
+                studentImgBg.width = studentImgBg.height * viewport.width / viewport.height;
+            }
+
             this.renderingService.renderThumbBackground(studentImgBg, data.pageInfo.currentDocNum, data.pageInfo.currentPage);
             this.renderingService.renderThumbBoard(canvas, data.pageInfo.currentDocNum, data.pageInfo.currentPage);
 
         })
 
 
-        /*-------------------------------------
-            monitoring canvas open 상태에서
-            학생이 draw 하는 내용에 대한 update.
-        ---------------------------------------*/
+
+        /************************************************************
+         * 학생 리스트 open 상태에서 학생이 draw 하는 내용에 대한 update.
+         ************************************************************/
         this.socket.on('send:monitoringCanvasDrawEvent', (data) => {
             console.log('<--- receive : monitoringCanvas One drawing Event from student.', data);
 
@@ -186,7 +214,6 @@ export class ComclassStudentComponent implements OnInit {
                     this.drawingService.drawThumb(data.drawingEvent, canvas, scale);
                 }
             }
-
         });
     }
 
@@ -200,10 +227,9 @@ export class ComclassStudentComponent implements OnInit {
      * @returns
      */
     async renderFileList() {
-        console.log('222222222222222222222222222222')
         // File List Background 그리기 : 각 문서의 1page만 그림
-
         // const numPages = this.viewInfoService.state.documentInfo[this.currentDocNum - 1].numPages;
+
         this.thumbArray = [];
         let thumbSize;
 
@@ -215,22 +241,11 @@ export class ComclassStudentComponent implements OnInit {
         };
 
         console.log(this.thumbArray)
-
-        await new Promise(res => setTimeout(res, 300));
+        await new Promise(res => setTimeout(res, 0));
         // for (let i = 0; i < this.student_monitoringRef.toArray().length; i++) {
         for (let i = 0; i < this.studentList.length; i++) {
-
-            await this.renderingService.renderThumbBackground(this.studentBgRef.toArray()[i].nativeElement, 1, 1);
-            await this.renderingService.renderThumbBoard(this.student_monitoringRef.toArray()[i].nativeElement, 1, 1);
-
-        };
-
-        for (let i = 0; i < this.studentList.length; i++) {
-            const canvas = (document.getElementById('student_monitoring' + this.studentDocInfo[i].studentName) as HTMLInputElement);
-            const studentImgBg = (document.getElementById('studentBg' + this.studentDocInfo[i].studentName) as HTMLInputElement);
-
-            await this.renderingService.renderThumbBackground(studentImgBg, this.studentDocInfo[i]?.currentDocNum,  this.studentDocInfo[i]?.currentPage);
-            await this.renderingService.renderThumbBoard(canvas, this.studentDocInfo[i]?.currentDocNum,  this.studentDocInfo[i]?.currentPage);
+            this.renderingService.renderThumbBackground(this.studentBgRef.toArray()[i].nativeElement, 1, 1);
+            this.renderingService.renderThumbBoard(this.student_monitoringRef.toArray()[i].nativeElement, 1, 1);
         };
 
         // 아래와 같은 방식도 사용가능(참고용)
@@ -238,7 +253,6 @@ export class ComclassStudentComponent implements OnInit {
         // this.thumRef.forEach((element, index) => {
         //   this.renderingService.renderThumbBackground(element.nativeElement, index + 1, 1); // element, doc Number, page Number
         // });
-
     };
 
 
