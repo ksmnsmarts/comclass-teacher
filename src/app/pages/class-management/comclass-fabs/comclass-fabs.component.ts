5ac2aff27767f98558c02f34626a7c2bbdc60eee
ljwoon <ljwoon@nsmarts.co.kr>
2022-07-14 14:48:06 +0900

    update

--- src/app/pages/class-management/comclass-fabs/comclass-fabs.component.ts
+++ src/app/pages/class-management/comclass-fabs/comclass-fabs.component.ts
@@ -3,43 +3,46 @@ import { Component, OnInit } from '@angular/core';
 import { ZoomService } from 'src/app/0.shared/services/zoom/zoom.service'
 
 import { ViewInfoService } from 'src/app/0.shared/store/view-info.service';
-
+import plusIcon from '@iconify/icons-akar-icons/plus';
+import minusIcon from '@iconify/icons-akar-icons/minus';
 
 @Component({
-  selector: 'app-comclass-fabs',
-  templateUrl: './comclass-fabs.component.html',
-  styleUrls: ['./comclass-fabs.component.scss']
+	selector: 'app-comclass-fabs',
+	templateUrl: './comclass-fabs.component.html',
+	styleUrls: ['./comclass-fabs.component.scss']
 })
 export class ComclassFabsComponent implements OnInit {
 
-  constructor(
-    private viewInfoService: ViewInfoService,
-    private zoomService: ZoomService
+	plusIcon = plusIcon
+	minusIcon = minusIcon
+	constructor(
+		private viewInfoService: ViewInfoService,
+		private zoomService: ZoomService
 
-  ) { }
+	) { }
 
-  ngOnInit(): void {
+	ngOnInit(): void {
 
-  }
+	}
 
 
-  /**
-   * Zoom Button에 대한 동작
-   * - viewInfoService의 zoomScale 값 update
-   *
-   * @param action : 'fitToWidth' , 'fitToPage', 'zoomIn', 'zoomOut'
-   */
-  clickZoom(action:any){
-    console.log(">> Click Zoom: ", action);
+	/**
+	 * Zoom Button에 대한 동작
+	 * - viewInfoService의 zoomScale 값 update
+	 *
+	 * @param action : 'fitToWidth' , 'fitToPage', 'zoomIn', 'zoomOut'
+	 */
+	clickZoom(action: any) {
+		console.log(">> Click Zoom: ", action);
 
-    const docNum = this.viewInfoService.state.pageInfo.currentDocNum;
-    const currentPage = this.viewInfoService.state.pageInfo.currentPage;
-    const prevZoomScale = this.viewInfoService.state.pageInfo.zoomScale;
+		const docNum = this.viewInfoService.state.pageInfo.currentDocNum;
+		const currentPage = this.viewInfoService.state.pageInfo.currentPage;
+		const prevZoomScale = this.viewInfoService.state.pageInfo.zoomScale;
 
-    const newZoomScale = this.zoomService.calcZoomScale(action, docNum, currentPage, prevZoomScale);
+		const newZoomScale = this.zoomService.calcZoomScale(action, docNum, currentPage, prevZoomScale);
 
-    this.viewInfoService.updateZoomScale(newZoomScale);
+		this.viewInfoService.updateZoomScale(newZoomScale);
 
-  }
+	}
 
 }
